e46e4fbea122924a3900842e478836104b8e71e9
Reggie <Reginaldtho@aol.com>
2024-06-23 21:35:04 -0700

    INDEX.JS UPDATE 1

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,6 @@ const initialCards = [
     link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg',
    },
 
-   { title:'Lake Louise',
-    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg'
-    },
-
     { title:'Lake Louise',
     link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg'
     },
