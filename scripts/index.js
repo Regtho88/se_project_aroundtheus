1677ce3402b8f8433c36a2043078be37d623249c
Reggie <Reginaldtho@aol.com>
2024-07-23 23:22:43 -0700

    new updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
-const initialCards = [
+const initialCards = [ 
+
    { name:'Yosemite Valley',
     link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg',
    },
@@ -22,7 +23,37 @@ const initialCards = [
     { name:'Lago di Braies',
     link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg',
     },
+];
+const buttonEdit = document.querySelector('#js-edit-button');
+const profileEditModal = document.querySelector('#profile-edit-modal');
+const closeEditForm = document.querySelector('#close-edit-btn');
 
-]
+const profileFormElement = profileEditModal.querySelector('.modal__form');
 
-console.log(initialCards);
+const nameInput = document.querySelector('.modal__heading');
+const jobInput = document.querySelector('.modal__description');
+
+const profileName = document.querySelector('#profile_name');
+const profileJob = document.querySelector('#profile_job');
+const cardTemplate = document.querySelector('#card-template').content;
+const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+const cardContainter = document.querySelector('.cards__list');
+
+
+profileEditButton.addEventListener('click', () => { 
+    profileTitleInput.value = profileTitle.textContent;
+
+    profileEditModal.classList.add('modal_opened');
+});
+
+
+profileCloseModal.addEventListener('click', () => {
+    profileEditModal.classList.remove('modal_opened');
+});
+
+
+
+
+
+
+
